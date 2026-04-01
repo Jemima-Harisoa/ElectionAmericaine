471868dee1b3738619c30e73e560410971252759
Jemima-Harisoa <jemimarazafimandimby@gmail.com>
2026-04-01 08:37:45 +0300

    feat: Update configuration and dependencies for FlightPHP integration

--- app/config.php
+++ app/config.php
@@ -1,6 +1,6 @@
 <?php
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'tp_validation');
+define('DB_HOST', 'db');
+define('DB_NAME', 'elections');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_CHARSET', 'utf8mb4');
